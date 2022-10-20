4abbe4692768aa39037d2581eb13e592a9829029
MyersP <Myers.Paul@Gmail.com>
2022-10-20 14:55:34 -0700

    Fixing LUA
    The LUA files do not need a change table so I am removing it.

--- Sounds/Silent Portable Refiner/Silent Portable Refiner.lua
+++ Sounds/Silent Portable Refiner/Silent Portable Refiner.lua
@@ -6,21 +6,5 @@ NMS_MOD_DEFINITION_CONTAINER =
 ["MOD_AUTHOR"]				= "Lowkie",
 ["NMS_VERSION"]				= "4.0",
 ["MOD_DESCRIPTION"]			= "Removes the sound associated with the Portable Refiner",
-["MODIFICATIONS"] 			= 
-	{
-		{
-			["MBIN_CHANGE_TABLE"] 	= 
-			{ 
-				{
-					["MBIN_FILE_SOURCE"] 	= {""},
-					["EXML_CHANGE_TABLE"] 	= 
-					{
-						{
-							["VALUE_CHANGE_TABLE"] 	={}
-						}
-					}
-				}	
-			}
-		}
-	}	
+["MODIFICATIONS"] 			=  {}	
 }
